44ee549517ead4897fa94e181bc0daaa721c8926
Abdurrahman Ansari <abdurrahmanansari03@gmail.com>
2023-08-31 13:42:16 +0530

    Created new file:   Hello World.py

---
+++ Hello World.py
@@ -0,0 +1,7 @@
+ #Program for printing helo World on the screen.
+print("Hello World")
+print("Hello\nWorld")
+print("Hello","World",sep="~")
+print("Hello\tWorld",end ="->")
+print("Hello World")
+ 
